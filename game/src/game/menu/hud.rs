25bc1701c2dfcbf627f4e18ad6197f7fe9308571
Guilherme Lampert <guilherme.ronaldo.lampert@gmail.com>
2025-11-07 15:36:19 +0700

    Hud menu placeholders.

--- game/src/game/menu/hud.rs
+++ game/src/game/menu/hud.rs
@@ -5,13 +5,22 @@ use super::*;
 // HUD -> Heads Up Display, AKA in-game menus
 // ----------------------------------------------
 
+type HudTilePlacement = TilePlacement;
+
+// TODO / WIP
 pub struct HudMenus {
-    // TODO / WIP
+    tile_placement: HudTilePlacement,
+    tile_palette: HudTilePalette,
+    tile_inspector: HudTileInspector,
 }
 
 impl HudMenus {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            tile_placement: HudTilePlacement::new(),
+            tile_palette: HudTilePalette::new(),
+            tile_inspector: HudTileInspector::new(),
+        }
     }
 }
 
@@ -20,9 +29,17 @@ impl GameMenusSystem for HudMenus {
         self
     }
 
-    fn tile_placement(&mut self) -> &mut TilePlacement { todo!() }
-    fn tile_palette(&mut self) -> &mut dyn TilePalette { todo!() }
-    fn tile_inspector(&mut self) -> Option<&mut dyn TileInspector> { todo!() }
+    fn tile_placement(&mut self) -> &mut TilePlacement {
+        &mut self.tile_placement
+    }
+
+    fn tile_palette(&mut self) -> &mut dyn TilePalette {
+        &mut self.tile_palette
+    }
+
+    fn tile_inspector(&mut self) -> Option<&mut dyn TileInspector> {
+        Some(&mut self.tile_inspector)
+    }
 }
 
 // ----------------------------------------------
@@ -32,3 +49,64 @@ impl GameMenusSystem for HudMenus {
 impl Save for HudMenus {}
 
 impl Load for HudMenus {}
+
+// ----------------------------------------------
+// HudTilePalette
+// ----------------------------------------------
+
+struct HudTilePalette {
+    // TODO / WIP
+}
+
+impl HudTilePalette {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl TilePalette for HudTilePalette {
+    fn on_mouse_button(&mut self,
+                       _button: MouseButton,
+                       _action: InputAction)
+                       -> UiInputEvent {
+        UiInputEvent::NotHandled
+    }
+
+    fn wants_to_place_or_clear_tile(&self) -> bool {
+        false
+    }
+
+    fn current_selection(&self) -> TilePaletteSelection {
+        TilePaletteSelection::None
+    }
+
+    fn clear_selection(&mut self) {
+    }
+}
+
+// ----------------------------------------------
+// HudTileInspector
+// ----------------------------------------------
+
+struct HudTileInspector {
+    // TODO / WIP
+}
+
+impl HudTileInspector {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl TileInspector for HudTileInspector {
+    fn on_mouse_button(&mut self,
+                       _button: MouseButton,
+                       _action: InputAction,
+                       _selected_tile: &Tile)
+                       -> UiInputEvent {
+        UiInputEvent::NotHandled
+    }
+
+    fn close(&mut self) {
+    }
+}
