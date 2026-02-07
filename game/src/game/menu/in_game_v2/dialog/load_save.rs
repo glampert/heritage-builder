b0a387fae43bf9c7e13b5fd821b6d6962f596f07
Guilherme Lampert <guilherme.ronaldo.lampert@gmail.com>
2026-02-07 14:45:43 +0700

    SaveGameHelper WIP.

--- game/src/game/menu/in_game_v2/dialog/load_save.rs
+++ game/src/game/menu/in_game_v2/dialog/load_save.rs
@@ -1,11 +1,18 @@
+use std::path::Path;
+use bitflags::bitflags;
+
 use super::*;
-use crate::{declare_dialog_menu};
+use crate::{
+    declare_dialog_menu,
+    game::{GameLoop, AUTOSAVE_FILE_NAME, DEFAULT_SAVE_FILE_NAME},
+};
 
 // ----------------------------------------------
 // LoadGame
 // ----------------------------------------------
 
 pub struct LoadGame {
+    helper: SaveGameHelper,
     menu: UiMenuRcMut,
 }
 
@@ -13,19 +20,9 @@ declare_dialog_menu! { LoadGame, "Load Game" }
 
 impl LoadGame {
     pub fn new(context: &mut UiWidgetContext) -> Self {
-        let widgets = ArrayVec::<UiWidgetImpl, 1>::new();
-
-        // TODO: Add widgets
-
-        Self {
-            menu: make_default_dialog_menu_layout(
-                context,
-                DialogMenuKind::LoadGame,
-                Self::TITLE,
-                DEFAULT_DIALOG_MENU_WIDGET_SPACING,
-                Some(widgets)
-            )
-        }
+        let helper = SaveGameHelper::new(SaveGameActions::Load);
+        let menu = helper.build_menu(context, Self::KIND, Self::TITLE);
+        Self { helper, menu }
     }
 }
 
@@ -34,6 +31,7 @@ impl LoadGame {
 // ----------------------------------------------
 
 pub struct SaveGame {
+    helper: SaveGameHelper,
     menu: UiMenuRcMut,
 }
 
@@ -41,19 +39,9 @@ declare_dialog_menu! { SaveGame, "Save Game" }
 
 impl SaveGame {
     pub fn new(context: &mut UiWidgetContext) -> Self {
-        let widgets = ArrayVec::<UiWidgetImpl, 1>::new();
-
-        // TODO: Add widgets
-
-        Self {
-            menu: make_default_dialog_menu_layout(
-                context,
-                DialogMenuKind::SaveGame,
-                Self::TITLE,
-                DEFAULT_DIALOG_MENU_WIDGET_SPACING,
-                Some(widgets)
-            )
-        }
+        let helper = SaveGameHelper::new(SaveGameActions::Save);
+        let menu = helper.build_menu(context, Self::KIND, Self::TITLE);
+        Self { helper, menu }
     }
 }
 
@@ -62,6 +50,7 @@ impl SaveGame {
 // ----------------------------------------------
 
 pub struct LoadOrSaveGame {
+    helper: SaveGameHelper,
     menu: UiMenuRcMut,
 }
 
@@ -69,18 +58,97 @@ declare_dialog_menu! { LoadOrSaveGame, "Load / Save" }
 
 impl LoadOrSaveGame {
     pub fn new(context: &mut UiWidgetContext) -> Self {
-        let widgets = ArrayVec::<UiWidgetImpl, 1>::new();
+        let helper = SaveGameHelper::new(SaveGameActions::Load | SaveGameActions::Save);
+        let menu = helper.build_menu(context, Self::KIND, Self::TITLE);
+        Self { helper, menu }
+    }
+}
 
-        // TODO: Add widgets
+// ----------------------------------------------
+// SaveGameActions / SaveGameHelper
+// ----------------------------------------------
 
-        Self {
-            menu: make_default_dialog_menu_layout(
-                context,
-                DialogMenuKind::LoadGame,
-                Self::TITLE,
-                DEFAULT_DIALOG_MENU_WIDGET_SPACING,
-                Some(widgets)
-            )
-        }
+bitflags! {
+    #[derive(Copy, Clone)]
+    struct SaveGameActions: u8 {
+        const Save = 1 << 0;
+        const Load = 1 << 1;
+    }
+}
+
+struct SaveGameHelper {
+    actions: SaveGameActions,
+}
+
+impl SaveGameHelper {
+    fn new(actions: SaveGameActions) -> Self {
+        Self { actions }
+    }
+
+    fn default_save_file_name(&self) -> String {
+        let default_file_name = {
+            if self.actions.intersects(SaveGameActions::Load) {
+                AUTOSAVE_FILE_NAME
+            } else {
+                DEFAULT_SAVE_FILE_NAME
+            }
+        };
+
+        // Remove extension.
+        Path::new(default_file_name)
+            .with_extension("")
+            .to_str().unwrap().into()
+    }
+
+    fn build_menu(&self,
+                  context: &mut UiWidgetContext,
+                  dialog_menu_kind: DialogMenuKind,
+                  heading_title: &str)
+                  -> UiMenuRcMut
+    {
+        let save_files = GameLoop::get()
+            .save_files_list()
+            .iter()
+            .map(|path| path.with_extension("").to_str().unwrap().into())
+            .collect::<Vec<String>>();
+
+        let save_files_list = UiItemList::new(
+            context,
+            UiItemListParams {
+                font_scale: DEFAULT_DIALOG_MENU_WIDGET_FONT_SCALE,
+                size: Some(Vec2::new(0.0, 250.0)), // Use whole parent window width minus margin, fixed height.
+                margin_left: 95.0,
+                margin_right: 100.0,
+                flags: UiItemListFlags::Border | 
+                       UiItemListFlags::TextInputField |
+                       UiItemListFlags::Scrollbars |
+                       UiItemListFlags::Scrollable,
+                current_item: None,
+                items: save_files, // TODO: need to update whenever file list changes!
+                on_selection_changed: UiItemListSelectionChanged::with_fn(
+                    |_save_files_list, _context| {
+                        //let selection_string = item_list.current_selection().unwrap_or_else(|| {
+                            //item_list.current_text_input_field().unwrap_or_default()
+                        //});
+
+                        //log::info!("Updated ItemList: '{}' [{:?}]",
+                            //selection_string,
+                            //item_list.current_selection_index());
+                    }
+                ),
+                ..Default::default()
+            }
+        );
+
+        let mut menu = make_default_dialog_menu_layout(
+            context,
+            dialog_menu_kind,
+            heading_title,
+            DEFAULT_DIALOG_MENU_WIDGET_SPACING,
+            Option::<Vec<UiWidgetImpl>>::None
+        );
+
+        menu.add_widget(save_files_list);
+        menu
     }
 }
